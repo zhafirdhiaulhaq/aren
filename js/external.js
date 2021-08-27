f970015027c942db4a1ee16dbbb7dee348bf250e
zhafirdhiaulhaq <62381344+zhafirdhiaulhaq@users.noreply.github.com>
2021-08-28 00:48:55 +0700

    initial and create a litle change

---
+++ js/external.js
@@ -0,0 +1,22 @@
+const nav = document.querySelector('nav');
+const ul = document.querySelector('ul');
+const txt = ul.querySelectorAll('.text-white');
+
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+        nav.classList.add('bg-light', 'shadow', 'navbar-light');
+        
+        for (i = 0; i < txt.length; i++) {
+            txt[i].classList.add('text-dark');
+            txt[i].classList.remove('text-white');
+        }
+        
+    } else {
+        nav.classList.remove('bg-light', 'shadow');
+
+        for (i = 0; i < txt.length; i++) {
+            txt[i].classList.remove('text-dark');
+        }
+    }
+});
