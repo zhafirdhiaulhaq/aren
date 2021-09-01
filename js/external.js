b048ec87213381399b41fd3c2de189221e847653
zhafirdhiaulhaq <62381344+zhafirdhiaulhaq@users.noreply.github.com>
2021-09-01 07:44:51 +0700

    update responsive all component at tablet-screen

--- js/external.js
+++ js/external.js
@@ -1,11 +1,15 @@
 const nav = document.querySelector('nav');
 const ul = document.querySelector('ul');
 const txt = ul.querySelectorAll('.text-white');
+const toggler = document.querySelector('.navbar-toggler');
 
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > 100) {
         nav.classList.add('bg-light', 'shadow', 'navbar-light');
+        // toggler.classList.add('custom-toggler');
+        // toggler.classList.remove('navbar-toggler');
+        // toggler.style.display = "none";
 
         for (i = 0; i < txt.length; i++) {
             txt[i].classList.add('text-dark');
@@ -14,16 +18,11 @@ window.addEventListener('scroll', function () {
 
     } else {
         nav.classList.remove('bg-light', 'shadow');
+        // toggler.classList.remove('custom-toggler');
+        // toggler.classList.add('navbar-toggler');
 
         for (i = 0; i < txt.length; i++) {
             txt[i].classList.remove('text-dark');
         }
     }
 });
-
-
-// $(window).resize(function () {
-//     If($(window).width() < 576){
-//         $('.col-7').removeClass('col-7');
-//     }
-// });
